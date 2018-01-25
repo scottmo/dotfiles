983f3ba28ddb69b481b0cdf5d476cee8240a2e41
scottmo <scottscmo@gmail.com>
2018-01-24 21:18:13 -0800

    Add gitconfig template and remove some of the git aliases in favor of the gitconfig aliases

--- shell/plugins/git.sh
+++ shell/plugins/git.sh
@@ -1,17 +1,9 @@
-# Log
-_git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-
-alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
-alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
-alias glg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-alias glgg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
-
 # Commit
 alias gcm='git commit --message'
 alias gamend='git commit --amend --reuse-message HEAD'
 alias gco='git checkout'
 alias gcpn='git cherry-pick --no-commit'
-alias gcp='git cherry-pick'
+alias gcp='git cherry-pick -x'
 alias gcr='git revert'
 alias greverttobase='git reset HEAD^'
 alias grevertlastcommit='git revert HEAD~'
@@ -30,16 +22,10 @@ gsync () {
     git pull upstream "$branch" && git push origin "$branch"
 }
 
-# stash
-gsshow () {
-    git stash show stash^{/$1} -p
-}
-
-gsapply () {
-    git stash apply stash^{/$1}
-}
-
-alias gssave='git stash save'
+# diff
+alias gdiff-unstaged='git diff'
+alias gdiff-staged='git diff --cached'
+alias gdiff-committed='git diff `thisb` origin/`thisb`'
 
 # branch
 alias gb='git branch'
@@ -56,6 +42,10 @@ thisb () {
 
 gpushbranch () {
     git push origin `thisb`
+}
+
+gpullbranch () {
+    git pull origin `thisb`
 }
 
 # util
