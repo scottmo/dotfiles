c18f6574f4f4508342fbf2d5c621d7cced57502f
scottmo <scottscmo@gmail.com>
2018-04-19 17:38:37 -0700

    Add check to avoid duplicate entries

--- shell/plugins/ruby.sh
+++ shell/plugins/ruby.sh
@@ -2,12 +2,14 @@
 
 # rbenv
 if [ -d "$HOME/.rbenv/bin" ]; then
-    export PATH="$HOME/.rbenv/bin:$PATH"
+    local rubypath="$HOME/.rbenv/bin"
+    [[ :$PATH: == *":$rubypath:"* ]] || PATH="$rubypath:$PATH"
     eval "$(rbenv init -)"
 else # use system default
     # install to the following path, no more sudo
     alias gemi="gem install --user-install"
     alias gemu="gem update --user-install"
 
-    export PATH="$HOME/.gem/ruby/2.0.0/bin:$PATH"
+    local rubypath="$HOME/.gem/ruby/2.0.0/bin"
+    [[ :$PATH: == *":$rubypath:"* ]] || PATH="$rubypath:$PATH"
 fi
