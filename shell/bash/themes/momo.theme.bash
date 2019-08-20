851b88d754a7f39d802c49f0ae98817e52a5c032
scottmo <scottscmo@gmail.com>
2019-08-20 11:45:25 -0500

    Remove color imports

--- shell/bash/themes/momo.theme.bash
+++ shell/bash/themes/momo.theme.bash
@@ -1,4 +1,11 @@
-source $DOTFILES/shell/colors.sh
+#!/usr/bin/env bash
+
+TXTRESET="\e[0m"
+ERROR_C="\e[0;31m"
+PATH_BRACKET_C="\e[1;33m"
+PATH_C="\e[1;32m"
+GIT_BRANCH_C="\e[1;35m"
+PROMPT_$="\e[1;34m"
 
 __parse_git_dirty() {
     [[ $(git status 2> /dev/null | tail -n1 | cut -c 1-17) != "nothing to commit" ]] && echo "*"
@@ -9,12 +16,12 @@ __parse_git_branch() {
 }
 
 __sadface() {
-    [ $? != 0 ] && echo -e "${TXTRED}\xE2\x98\xB9"
+    [ $? != 0 ] && echo -e "${ERROR_C}\xE2\x98\xB9"
 }
 
 __prompt_command() {
     __sadface
-    PS1="\n\[${BLDYLW}\]<[ \[${BLDGRN}\]\w\[${BLDYLW}\] ]> \[${BLDPUR}\]\$(__parse_git_branch)\[${BLDBLU}\]\n\$ \[${TXTRESET}\]"
+    PS1="\n\[${PATH_BRACKET_C}\]<[ \[${PATH_COLOR}\]\w\[${PATH_BRACKET_C}\] ]> \[${GIT_BRANCH_C}\]\$(__parse_git_branch)\[${PROMPT_$}\]\n\$ \[${TXTRESET}\]"
 }
 
 PROMPT_COMMAND=__prompt_command
--- shell/bash/themes/simple_path.theme.bash
+++ shell/bash/themes/simple_path.theme.bash
@@ -1,4 +1,11 @@
-source $DOTFILES/shell/colors.sh
+#!/usr/bin/env bash
+
+TXTRESET="\e[0m"
+ERROR_C="\e[0;31m"
+DASH_C="\e[1;34m"
+ARROW_1_C="\e[1;31m"
+ARROW_2_C="\e[1;33m"
+ARROW_3_C="\e[1;32m"
 
 __sadface() {
     [ $? != 0 ] && echo -e "${TXTRED}\xE2\x98\xB9"
@@ -7,8 +14,7 @@ __sadface() {
 __prompt_command() {
     __sadface
     dir=$(basename $(dirname $PWD))/$(basename $PWD)
-    PS1="\[${BLDBLU}\]- \[${BLDRED}\]$dir >\[${BLDYLW}\]>\[${BLDGRN}\]> \[${TXTRESET}\]"
+    PS1="\[${DASH_C}\]- \[${ARROW_1_C}\]$dir >\[${ARROW_2_C}\]>\[${ARROW_3_C}\]> \[${TXTRESET}\]"
 }
 
 PROMPT_COMMAND=__prompt_command
-
