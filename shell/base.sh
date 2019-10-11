148de12816ea745d7ec40638e372c14ac092241b
scottmo <scottscmo@gmail.com>
2019-10-11 16:31:52 -0500

    unset file variable

--- shell/base.sh
+++ shell/base.sh
@@ -8,15 +8,11 @@ export DOTFILES=$HOME/dotfiles
 #
 # source external files
 #
-. $DOTFILES/shell/lib.sh
+# . $DOTFILES/shell/lib.sh
 for file in $DOTFILES/shell/plugins/*.sh; do
     . $file
 done
-
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
-
 for file in $DOTFILES/_local_/*.sh; do
     . $file
 done
-
+unset file
