ed9ea91dc86115d3b86edec095792f46e7d53b68
Shaocong Mo <smo@salesforce.com>
2020-04-12 13:34:11 -0500

    rename DOTFILES to dotfiles

--- shell/base.sh
+++ shell/base.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-export DOTFILES=$HOME/dotfiles
+export dotfiles=$HOME/dotfiles
 
-export PATH="$HOME/bin:$DOTFILES/bin:$PATH"
+export PATH="$HOME/bin:$dotfiles/bin:$PATH"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 
 # plugins
-for file in $DOTFILES/shell/plugins/*.sh; do
+for file in $dotfiles/shell/plugins/*.sh; do
     . $file
 done
 unset file
