7660a487591ad3df2bfa0c35d979885388dc69d6
Shaocong Mo <smo@salesforce.com>
2020-04-13 20:46:43 -0500

    update theme to highlight command

--- shell/bash/theme.bash
+++ shell/bash/theme.bash
@@ -16,7 +16,7 @@ function __parse_git_branch {
     is_git_dirty=`[[ $(git status 2> /dev/null | tail -n1 | cut -c 1-17) != "nothing to commit" ]] && echo "*"`
     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$is_git_dirty/"
 }
-__prompt_status() {
+function __prompt_status {
     local symbols=()
 
     [[ $RETVAL -ne 0 ]] && symbols+="$(tput setaf 1)(╯°□°）╯︵ ┻━┻" # bad result
@@ -28,19 +28,24 @@ __prompt_status() {
 function __theme_simple {
     # $ - path >>>
     local dir=$(basename $(dirname $PWD))/$(basename $PWD)
-    PS1="$(tput bold)$(tput setaf 4)-$(tput setab 6)$(tput setaf 0) $dir $(tput sgr0)$PROMPT_ARROW"
+    PS1="$(tput bold)$(tput setaf 4)- $dir $PROMPT_ARROW"
 }
 function __theme_momo {
     # <[ path ]> git_branch dirty
     # $
-    local dir="$(tput setaf 3)<[$(tput setab 3) $(tput setaf 0)\w $(tput sgr0)$(tput bold)$(tput setaf 3)]>"
-    PS1="$(tput bold)\n$dir $(tput setaf 6)\$(__parse_git_branch)$(tput setaf 4)\n\$ $(tput sgr0)"
+    local dir="$(tput setaf 2)<[ \w ]>$(tput sgr0)"
+    PS1="$(tput bold)$dir $(tput setaf 6)\$(__parse_git_branch)$(tput setaf 4)\n\$ "
+}
+function __highlight_command {
+    PS1="$PS1$(tput setab 3)$(tput setaf 0)"
+    PS0="$(tput sgr0)"
 }
 function __prompt_command {
     RETVAL=$?
     __prompt_status
     __theme_momo
     # __theme_simple
+    __highlight_command
     echo -ne "\033]0;${PWD##*/}\007" # set title to current folder name
 }
 
