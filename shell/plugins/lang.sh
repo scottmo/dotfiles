0ae43bada687f0ca4a4518ad52bf38d3a86e5409
Shaocong Mo <smo@salesforce.com>
2020-12-26 13:29:59 -0600

    Update lang version manager setup

--- shell/plugins/lang.sh
+++ shell/plugins/lang.sh
@@ -4,21 +4,10 @@
 # asdf
 #
 if [ -d "$HOME/.asdf/" ]; then
-    source ~/.asdf/asdf.sh
-    source ~/.asdf/completions/asdf.bash
+    source "$(brew --prefix asdf)/asdf.sh"
 fi
-
-#
-# ruby
-#
-[ -d "$HOME/.rbenv/shims" ] && export PATH="$HOME/.rbenv/shims:$PATH"
 
 #
 # rust
 #
 [ -d "$HOME/.cargo/bin" ] && export PATH="$HOME/.cargo/bin:$PATH"
-
-#
-# node
-#
-[ -d "$HOME/n" ] && export N_PREFIX="$HOME/n" && export PATH="$N_PREFIX/bin:$PATH"
