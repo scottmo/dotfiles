091a598ba29a48fb93dfe522ccde22df685bb312
scottmo <scottscmo@gmail.com>
2021-05-25 12:51:44 -0500

    feat: add zsh plugins

--- shell/zsh/zinit.zsh
+++ shell/zsh/zinit.zsh
@@ -3,6 +3,14 @@
 #
 # load plugin manager
 #
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
 source $HOME/.zinit/bin/zinit.zsh
 # if already compinit, load completion manually
 autoload -Uz _zinit
@@ -11,12 +19,21 @@ autoload -Uz _zinit
 #
 # plugins
 #
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zinit-zsh/z-a-rust \
+    zinit-zsh/z-a-as-monitor \
+    zinit-zsh/z-a-patch-dl \
+    zinit-zsh/z-a-bin-gem-node
+
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma/fast-syntax-highlighting
+zinit light olets/zsh-abbr
 
-zinit load zdharma/history-search-multi-word
+# zinit load zdharma/history-search-multi-word
 
 zinit light Aloxaf/fzf-tab
 
