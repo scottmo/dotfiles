90ace9bc67d2996e04ed54e0c23fa5dfc7f46c31
scottmo <scottscmo@gmail.com>
2021-07-11 15:12:39 -0500

    rm zsh-abbr

--- shell/zsh/zinit.zsh
+++ shell/zsh/zinit.zsh
@@ -31,7 +31,6 @@ zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma/fast-syntax-highlighting
-zinit light olets/zsh-abbr
 
 # zinit load zdharma/history-search-multi-word
 
