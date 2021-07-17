763a14496a6ab7c2ca6901cebe4ee59df3792d20
scottmo <scottscmo@gmail.com>
2021-07-17 11:54:13 -0500

    change to double quotes

--- zsh/plugins/git.sh
+++ zsh/plugins/git.sh
@@ -2,31 +2,31 @@
 
 ! [ $(command -v git) ] && return 0
 
-alias gcm='git commit --message'
-alias gamend='git commit --amend --reuse-message HEAD'
-alias gcpn='git cherry-pick --no-commit'
-alias gcpx='git cherry-pick -x'
-alias gcp='git cherry-pick'
-alias gcr='git revert'
-alias greverttobase='git reset HEAD^'
-alias grevertlastcommit='git revert HEAD~'
+alias gcm="git commit --message"
+alias gamend="git commit --amend --reuse-message HEAD"
+alias gcpn="git cherry-pick --no-commit"
+alias gcpx="git cherry-pick -x"
+alias gcp="git cherry-pick"
+alias gcr="git revert"
+alias greverttobase="git reset HEAD^"
+alias grevertlastcommit="git revert HEAD~"
 
-alias gst='git status'
+alias gst="git status"
 
-alias ggls='git ls-files'
+alias ggls="git ls-files"
 
-alias gms='git merge --squash'
+alias gms="git merge --squash"
 
 # branch
-alias gb='git branch | cat'
-alias gbc='git checkout -b'
-alias gbx='git branch -d'
-alias gbX='git branch -D'
-alias gbm='git branch -m'
-alias gbM='git branch -M'
-alias gco='git checkout'
+alias gb="git branch | cat"
+alias gbc="git checkout -b"
+alias gbx="git branch -d"
+alias gbX="git branch -D"
+alias gbm="git branch -m"
+alias gbM="git branch -M"
+alias gco="git checkout"
 
 alias thisb="git rev-parse --abbrev-ref HEAD"
 
 # util
-alias gshrink='git gc --prune=now --aggressive'
+alias gshrink="git gc --prune=now --aggressive"
