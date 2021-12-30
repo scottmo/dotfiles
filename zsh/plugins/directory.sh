6a7de6130d5c0a8677285bc425554f753e9c5089
scottmo <scottscmo@gmail.com>
2021-12-30 16:29:23 -0600

    mv some of the utils to python script

--- zsh/plugins/directory.sh
+++ zsh/plugins/directory.sh
@@ -10,18 +10,3 @@ alias l='ls -1'
 alias ll='ls -l'
 alias la='ls -la'
 alias lsdir='echo `ls -l | grep "^d" | awk "{ print $9 }" | tr -d "/"`'
-
-# numFiles: Count of non-hidden files in current dir
-alias numFiles='echo $(ls -1 | wc -l)'
-
-# qfind: Quickly search for file
-alias qfind="find . -name "
-
-function cmkdir {
-    mkdir -p "$1";cd "$1";
-}
-
-
-if ! type tree &> /dev/null; then
-    alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
-fi
