1c824b4bea74216a11856dc7639bdff236770bdf
scottmo <scottscmo@gmail.com>
2021-12-30 18:23:11 -0600

    rm language specific setup

--- fish/config.fish
+++ fish/config.fish
@@ -24,17 +24,6 @@
         echo (prompt_pwd)
     end    
 # }}}
-# asdf {{{
-    # https://github.com/asdf-vm/asdf
-    test -d ~/.asdf && source ~/.asdf/asdf.fish
-# }}}
-# fnm {{{
-    # https://github.com/Schniz/fnm
-    test -d ~/.fnm && fnm env | source
-# }}}
-# rust {{{
-    test -d ~/.cargo/bin && fish_add_path ~/.cargo/bin
-# }}}
 # source external files {{{
     # plugins from other shell
     if functions -q replay
