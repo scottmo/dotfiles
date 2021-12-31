67d6c5abe1f7ac0286fdd4fe13b21ebb6714b286
scottmo <scottscmo@gmail.com>
2021-12-30 18:28:08 -0600

    rm old functions

--- zsh/plugins/osx.sh
+++ zsh/plugins/osx.sh
@@ -10,24 +10,3 @@ function cdf {
         echo 'No Finder window found' >&2
     fi
 }
-
-function finderlink {
-    if [ -z "$1" ]; then
-        echo "First argument should be path of file to link to"
-    else
-        osascript -e 'tell application "Finder" to make alias file to POSIX file "'$1'" at POSIX file "'$PWD'"'
-    fi
-}
-
-function screensaver {
-    open -a ScreenSaverEngine
-}
-
-function showhidden {
-    if [ "$1" != "YES" ] && [ "$1" != "NO" ]; then
-        echo "First argument needs to be either YES or NO"
-    else
-        defaults write com.apple.finder AppleShowAllFiles $1
-        killall Finder /System/Library/CoreServices/Finder.app
-    fi    
-}
