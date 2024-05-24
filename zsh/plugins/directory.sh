2cd6c9a543f34e7bc6523967cb12e007d08e0640
scottmo <scottscmo@gmail.com>
2024-05-24 15:14:28 -0500

    use lsd if available

--- zsh/plugins/directory.sh
+++ zsh/plugins/directory.sh
@@ -10,3 +10,7 @@ alias l='ls -1'
 alias ll='ls -l'
 alias la='ls -la'
 alias lsdir='echo `ls -l | grep "^d" | awk "{ print $9 }" | tr -d "/"`'
+
+if command -v lsd &> /dev/null; then
+    alias ls="lsd"
+fi
