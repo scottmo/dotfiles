76ea36f74336ebbe8b52ed889c96118f6479af8a
scottmo <scottscmo@gmail.com>
2024-08-27 16:59:02 -0500

    feat: trash util for mac

--- zsh/plugins/osx.sh
+++ zsh/plugins/osx.sh
@@ -15,5 +15,16 @@ function sms {
     osascript -e 'tell application "Messages" to send "'$2'" to participant "'$1'" of (1st account whose service type = SMS)'
 }
 
+function trash() {
+    if [[ $# ]]; then
+        a=()
+        for f in "$@"; do
+            a+=("$(realpath "$f")")
+        done
+        f=$(printf "\",POSIX file \"%s" "${a[@]}")\"
+        osascript -ss -e"tell app \"Finder\" to delete {${f:2}}" 1>/dev/null
+    fi
+}
+
 alias displaysleep="pmset displaysleepnow"
 alias thermlog="pmset -g thermlog"
