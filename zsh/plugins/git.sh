eb16ffdaf320e45c2a5686d4f4309858576f5774
scottmo <scottscmo@gmail.com>
2024-10-15 11:41:28 -0500

    refactor: mv git aliases out of gitconfig

--- zsh/plugins/git.sh
+++ zsh/plugins/git.sh
@@ -2,38 +2,60 @@
 
 ! [ $(command -v git) ] && return 0
 
+# undo
+alias gunstagefile="git reset -q HEAD --"
+alias greverttobase="git reset HEAD^"
+alias grevertlastcommit="git revert HEAD~"
+alias grevertcommit="git reset --mixed HEAD~"
+alias grevertunstaged="git checkout ."
+alias grevertall="git reset --hard"
+alias greverttohead="git reset --hard HEAD^"
+alias grevertclean="!git reset --hard HEAD && git clean -d -f"
+greverttoremote() { git reset --hard origin/$(gbthis) }
+
+# diff
+alias gdiffstaged="git diff --cached"
+alias gdiffparent="git diff HEAD^ HEA"
+gdiffremote() { git diff $(gbthis) origin/$(gbthis) }
+
+# commit
 alias gcm="git commit --message"
 alias gamend="git commit --amend --reuse-message HEAD"
 alias gcpn="git cherry-pick --no-commit"
 alias gcpx="git cherry-pick -x"
 alias gcp="git cherry-pick"
-alias gcr="git revert"
-alias greverttobase="git reset HEAD^"
-alias grevertlastcommit="git revert HEAD~"
+alias gms="git merge --squash"
 
 alias gst="git status"
 alias gss="git status -s"
 
-alias ggls="git ls-files"
-
-alias gms="git merge --squash"
-
-# branch
-alias gb="git branch | cat"
-alias gbc="git checkout -b"
-alias gbx="git branch -d"
-alias gbX="git branch -D"
-alias gbm="git branch -m"
-alias gbM="git branch -M"
-alias gco="git checkout"
-
-alias thisb="git rev-parse --abbrev-ref HEAD"
-
 alias gps="git push"
 alias gp="git pull"
 
+# branch
+alias gb="git branch | cat"
+alias gbnew="git checkout -b"
+alias gbdelete="git branch -D"
+alias gbrename="git branch -m"
+alias gco="git checkout"
+alias gbtrack="git branch --set-upstream-to"
+alias gbuntrack="git rm -r --cached"
+alias gbthis="git rev-parse --abbrev-ref HEAD"
+
+alias gremotes="git remote -v"
+
+alias ggls="git ls-files"
+
+# history
+alias ghistory="git log -10 --format=format:'%Cgreen%h %Creset%s (%aN, %ar)'"
+alias ghistoryfile="git log --follow -p -w"
+alias ghistorymerge="git log --ancestry-path --merges"
+alias ghistorygraph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+
 # util
 alias gshrink="git repack -a -d -f --depth=250 --window=250"
+gsync() { git pull upstream $1 && git push origin $1 }
+gcheckoutpr() { git fetch upstream pull/$1/head:$2 && git checkout $2 }
 
 # commit template
 gfeat() { git commit -m "feat: $1" }
